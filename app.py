e475187426d59736469d39c63229805d19d81b33
codjoero <rownee2001@gmail.com>
2018-10-04 14:56:15 +0300

    feat(modules and endpoints): Add modules fucntionality
    Updated all task endpoints and tested with postman
    
    [Deliveries #160949220]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, make_response
 from tasks import Tasks
 from accounts import Accounts
 
@@ -7,49 +7,68 @@ app = Flask(__name__)
 account = Accounts()
 task = Tasks()
 
-@app.route('/todo/api/v0.1/tasks', methods=['POST'])
-def create_user():
-    return jsonify({'tasks': task.show_all})
+""" 
+Error Handlers 
+"""
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
 
-@app.route('/todo/api/v0.1/tasks', methods=['PUT'])
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+""" 
+Accounts Endpoits
+"""
+@app.route('/todo/api/v0.1/users', methods=['POST'])
+def create_user():
+    pass
+
+@app.route('/todo/api/v0.1/users', methods=['PUT'])
 def login_user():
     pass
 
-@app.route('/todo/api/v0.1/tasks/<int:task_id>', methods=['PUT'])
+@app.route('/todo/api/v0.1/users/<int:task_id>', methods=['PUT'])
 def logout_user():
     pass
 
-@app.route('/todo/api/v0.1/tasks', methods=['DELETE'])
+@app.route('/todo/api/v0.1/users/<int:task_id>', methods=['DELETE'])
 def delete_user():
     pass
 
+""" 
+Tasks Endpoints
+"""
 @app.route('/todo/api/v0.1/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': task.show_all})
+    return jsonify({'tasks': task.show_all()})
+
 
 @app.route('/todo/api/v0.1/tasks', methods=['POST'])
 def create_task():
-    pass
+    return jsonify({'task': task.create_task()}), 201
+
+
+@app.route('/todo/api/v0.1/tasks/<int:task_id>', methods=['PUT'])
+def mark_finished(task_id):
+    return jsonify({'task': task.mark_finished(task_id)})
+
 
 @app.route('/todo/api/v0.1/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task():
-    pass
+def delete_task(task_id):
+    return jsonify({'result': task.delete_task(task_id)})
 
-@app.route('/todo/api/v0.1/tasks', methods=['PUT'])
+
+@app.route('/todo/api/v0.1/tasks/deleted/<int:task_id>', methods=['POST'])
+def undelete_task(task_id):
+    return jsonify({'result': task.undelete_task(task_id)}), 201
+
+
+@app.route('/todo/api/v0.1/tasks/del/all', methods=['DELETE'])
 def delete_all():
-    pass
+    return jsonify({'result': task.delete_all()})
 
-@app.route('/todo/api/v0.1/tasks/<int:task_id>', methods=['PUT'])
-def mark_finished():
-    pass
-
-@app.route('/todo/api/v0.1/tasks/<int:task_id>', methods=['PUT'])
-def unmark_finished():
-    pass
-
-@app.route('/todo/api/v0.1/tasks/<int:task_id>', methods=['PUT'])
-def undeleted_task():
-    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tasks.py
+++ tasks.py
@@ -1,25 +1,74 @@
-from flask import request, jsonify
+from flask import request, jsonify, abort
 
 class Tasks:
 
     def __init__(self):
-        self.user_tasks = []
+        self.user_tasks = [
+            {
+            'id': 1,
+            'title': 'Gym',
+            'items': 'shoes, towel, yoga mat, getorade',
+            'finished': False
+            }
+        ]
         self.deleted_tasks = []
-
-    def create_task(self):
-        pass
-
-    def delete_task(self):
-        pass
-
-    def delete_all(self):
-        pass
-
-    def mark_finished(self):
-        pass
-
-    def recover_deleted(self):
-        pass
 
     def show_all(self):
         return self.user_tasks
+
+    def create_task(self):
+        if not request.json or not 'title' in request.json:
+            abort(400)
+        task = {
+            'id': self.user_tasks[-1]['id'] + 1,
+            'title': request.json['title'],
+            'items': request.json.get('items', ""),
+            'finished': False
+        }
+        self.user_tasks.append(task)
+        return task
+
+    def mark_finished(self, task_id):
+        task = [task for task in self.user_tasks 
+        if task['id'] == task_id]
+
+        if len(task) == 0:
+            abort(404)
+        if not request.json:
+            abort(400)
+        if 'finished' in request.json and \
+        type(request.json['finished']) is not bool:
+            abort(400)
+        task[0]['finished'] = request.json.get('finished',
+                                task[0]['finished'])
+        return task
+    
+
+    def delete_task(self,task_id):
+        task = [task for task in self.user_tasks 
+        if task['id'] == task_id]
+
+        if len(task) == 0:
+            abort(404)
+        self.deleted_tasks.append(task[0])
+        self.user_tasks.remove(task[0])
+        return 'Deleted'
+
+
+    def undelete_task(self, task_id):
+        task = [task for task in self.deleted_tasks 
+        if task['id'] == task_id]
+
+        if len(task) == 0:
+            abort(404)
+        self.user_tasks.append(task[0])
+        self.deleted_tasks.remove(task[0])
+        return 'Recovered'
+
+    def delete_all(self):
+        self.user_tasks[:] = []
+        return 'All Deleted'
+
+    
+
+    
